a3ddfe09d8b602a3a94471c82edf5dfaba322282
pranayk36 <pranayk36@gmail.com>
2026-04-12 14:29:05 +0000

    Breaking test

--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -8,6 +8,6 @@ public class AppTest {
     @Test
     public void testAdd() {
         App app = new App();
-        assertEquals(5, app.add(2, 3));
+        assertEquals(6, app.add(2, 3));
     }
 }
